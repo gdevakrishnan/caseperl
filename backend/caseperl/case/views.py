6f4819ff4d3854ddb9c1f691f460d1f2f24144d1
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-10-22 22:35:29 +0530

    Urls setup and views

--- backend/caseperl/case/views.py
+++ backend/caseperl/case/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def home (request):
+    data = {
+        "message": "Server Running..."
+    }
+    return JsonResponse(data)
