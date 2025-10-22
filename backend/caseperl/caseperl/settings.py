bc1ab9992db2a0a40e96734f8a5946c00a427017
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-10-22 23:07:39 +0530

    Restframework integration

--- backend/caseperl/caseperl/settings.py
+++ backend/caseperl/caseperl/settings.py
@@ -37,9 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'case',
     'users'
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
